cf78fcb1ea07518e575fb864b78f4c77a69dc7eb
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-04-29 19:53:58 -0300

    feat: add home page

---
+++ src/app/Home.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import ShieldIcon from '../shared/icons/ShieldIcon';
+import {
+  PasswordInputPanel,
+  PasswordGeneratorPanel,
+  PasswordGenerateButton,
+  usePassword,
+  usePasswordGenerator,
+  usePasswordStrength,
+} from '../features/password';
+
+export default function Home() {
+  const { password, setPassword, results, normalizedScore } = usePassword();
+  const { options, setOptions, generatePassword } = usePasswordGenerator({
+    setPassword,
+  });
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOptions = () => setIsOpen((prev) => !prev);
+
+  const handleOptionChange = (key, value) => {
+    setOptions((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
+  return (
+    <main className="flex flex-col p-4 bg-bg h-max w-full items-center justify-center">
+      <div className="max-w-xl text-center mb-2">
+        <h1 className="text-3xl font-semibold text-primary-600 mb-3">
+          Generador y Evaluador de Contraseñas
+        </h1>
+        <p className="text-muted">
+          Crea o pega una contraseña para verificar su seguridad al instante con
+          estándares de nivel bancaria.
+        </p>
+      </div>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="w-full max-w-lg min-h-1/2 bg-surface shadow-elev rounded-lg p-6 flex flex-col gap-4"
+      >
+        <h1 className="flex flex-row gap-1 text-primary-400 font-bold">
+          <ShieldIcon className="size-6 text-primary-600" /> Control de
+          Seguridad
+        </h1>
+
+        <PasswordInputPanel
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          results={results}
+          normalizedScore={normalizedScore}
+        />
+
+        <PasswordGenerateButton
+          onClick={() => generatePassword()}
+          toggleOptions={toggleOptions}
+          isOpen={isOpen}
+        />
+        {isOpen && (
+          <>
+            <hr className="my-1 border-muted/20 border-t-2" />
+            <PasswordGeneratorPanel
+              options={options}
+              onOptionChange={handleOptionChange}
+              onGenerate={(generated) => setPassword(generated)}
+            />
+          </>
+        )}
+        <p className="flex flex-row text-xs self-center gap-2 items-center">
+          <ShieldIcon className="size-3" /> Tu contraseña no se guarda en ningún
+          momento.
+        </p>
+      </form>
+    </main>
+  );
+}
