5ef3cc27a306addc02321fe6b234588942f149e4
Nicolás Fenoglio <57574930+fnAlfa@users.noreply.github.com>
2026-04-30 20:50:43 -0300

    refactor: link to portafolio added to footer.

--- src/features/layout/Footer.jsx
+++ src/features/layout/Footer.jsx
@@ -3,7 +3,18 @@ import GitHubIcon from '../../shared/icons/GitHubIcon';
 function Footer() {
   return (
     <footer className="bg-bg text-muted py-4 px-4 flex items-center justify-between shadow-elev sticky top-0 z-10">
-      <p>&copy; 2026 - Nicolás Fenoglio. Trabajo Practico 2.</p>
+      <p>
+        &copy; 2026.{' '}
+        <a
+          href="https://nicofeno.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:cursor-pointer hover:underline hover:text-primary transition-colors"
+        >
+          Nicolás Fenoglio, estudiante de Ingenieria en Sistemas de la
+          Información.
+        </a>
+      </p>
       <p>
         <a
           className="flex items-center gap-1 hover:cursor-pointer hover:underline hover:text-primary transition-colors"
